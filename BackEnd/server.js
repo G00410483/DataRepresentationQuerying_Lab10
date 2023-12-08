d8d4533761c140ccf1efa6c7f38b082690ac2845
David Vlasic <dvlasic000@gmail.com>
2023-12-08 09:45:15 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,90 +1,98 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
 const cors = require('cors');
 
+// Serve the static files from the React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 const bodyParser = require("body-parser");
 
-//Here we are configuring express to use body-parser as middle-ware.
+// Configure express to use body-parser as middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-// getting-started.js
+// MongoDB setup
 const mongoose = require('mongoose');
 
+// Connect to MongoDB Atlas cluster
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb+srv://admin:admin@martinscluster.w5rtkz0.mongodb.net/DB14?retryWrites=true&w=majority');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+// Define the book schema and model
 const bookSchema = new mongoose.Schema({
-  title:String,
-  cover:String,
-  author:String
-})
+  title: String,
+  cover: String,
+  author: String
+});
 
 const bookModel = mongoose.model('dfgdfgdfgdfg5r5645634fggh', bookSchema);
 
-app.delete('/api/book/:id',async (req, res)=>{
-  console.log("Delete: "+req.params.id);
+// API routes
+
+// Delete a book by ID
+app.delete('/api/book/:id', async (req, res) => {
+  console.log("Delete: " + req.params.id);
 
   let book = await bookModel.findByIdAndDelete(req.params.id);
   res.send(book);
-})
+});
 
+// Update a book by ID
+app.put('/api/book/:id', async (req, res) => {
+  console.log("Update: " + req.params.id);
 
-app.put('/api/book/:id', async(req, res)=>{
-  console.log("Update: "+req.params.id);
-
-  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
+  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.send(book);
-})
+});
 
+// Create a new book
+app.post('/api/book', (req, res) => {
+  console.log(req.body);
 
-app.post('/api/book', (req,res)=>{
-    console.log(req.body);
+  bookModel.create({
+    title: req.body.title,
+    cover: req.body.cover,
+    author: req.body.author
+  })
+    .then(() => { res.send("Book Created"); })
+    .catch(() => { res.send("Book NOT Created"); });
+});
 
-    bookModel.create({
-      title:req.body.title,
-      cover:req.body.cover,
-      author:req.body.author
-    })
-    .then(()=>{ res.send("Book Created")})
-    .catch(()=>{ res.send("Book NOT Created")});
-
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/api/books', async(req, res)=>{
-    
+// Get all books
+app.get('/api/books', async (req, res) => {
   let books = await bookModel.find({});
   res.json(books);
-})
+});
 
-app.get('/api/book/:identifier',async (req,res)=>{
+// Get a book by identifier
+app.get('/api/book/:identifier', async (req, res) => {
   console.log(req.params.identifier);
 
   let book = await bookModel.findById(req.params.identifier);
   res.send(book);
-})
+});
 
+// Handle any requests that don't match the routes above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../build/index.html'));
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
